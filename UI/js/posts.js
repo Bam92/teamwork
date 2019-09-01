e2e174b30e4e35682d422777f5cf6ebd43901331
abelmbula <abelmbula@bam.com>
2019-09-01 11:06:13 +0200

    ft(dashboard):add detail page for an article
    From the dashboard employee can click on `Read article` to read the entire article.
    
    [#168223842]

--- UI/js/posts.js
+++ UI/js/posts.js
@@ -7,27 +7,27 @@ const posts = [
     {
         id: 1,
         img_url: '../images/posts/3.jpg',
-        title: 'My first post',
+        title: 'What does a great software engineer look like?',
         tags: ['rwanda', 'andela'],
-        message: 'Ouh!!! Awesome. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt esse quae libero odio totam facilis, officia maxime obcaecati ipsum ad vero similique natus quidem id excepturi fugit distinctio iste corporis!',
+        message: '<p>I should nuance the following with a note: not all organisations make it easy for software engineers to do their job well.</p> All the advice below comes with a big caveat: if management doesn\’t do their job right, or if the organisation doesn\’t see the value of good software engineering practices, chances are you won\’t be able to be a great software engineer. That\’s a much deeper issue, and not necessarily something you can fix (aside from changing jobs, if that\’s in the realm of possibility for you). Also, a lot of this advice doesn\’t apply to solo developers. Without further ado, here are a few critical soft skills I\’d expect from a great software engineer.',
         date: '21 Aug 2019 05:15:02 GMT',
         author: 'Sarah'
     },
     {
         id: 2,
         img_url: '../images/posts/2.jpg',
-        title: 'Just About Andela',
+        title: 'Angular basics',
         tags: ['one', 'andela'],
-        message: 'Lorem, ipsum dolor sit amet conse elit. Deserunt esse quae libero odio totam facilis, officia maxime obcaecati ipsum ad vero similique natus quidem id excepturi fugit distinctio iste corporis!',
+        message: 'Lorem, ipsum dolor sit amet conse elit. <p>Deserunt esse quae libero odio totam facilis, officia maxime obcaecati ipsum ad vero similique natus quidem id excepturi fugit distinctio iste corporis!</p>',
         date: '21 Aug 2019 07:28:00 GMT',
         author: 'Patience'
     },
     {
         id: 3,
-        img_url: '../images/posts/1.jpg',
+        img_url: '../images/posts/1.png',
         title: 'Starting a new job',
         tags: ['job', 'andela', 'congo'],
-        message: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt esse quae libero odio totam facilis, officia maxime obcaecati ipsum ad vero similique natus quidem id excepturi fugit distinctio iste corporis!',
+        message: 'Lorem, ipsum <b>dolor sit amet </b>consectetur adipisicing elit. Deserunt esse quae libero odio totam facilis, officia maxime obcaecati ipsum ad vero similique natus quidem id excepturi fugit distinctio iste corporis!',
         date: '22 Aug 2019 07:28:00 GMT',
         author: 'Sarah'
     }
