72cd612035117e3a656550151dfaef1cb8e10adb
abelmbula <abelmbula@bam.com>
2019-09-03 21:26:48 +0200

    :sparkles: ft(api):implement POST /api/v1/auth/signup
    Employee can create an account and get back a token
    
    [#168256525]

---
+++ server/src/routes/auth.js
@@ -0,0 +1,28 @@
+/**
+* @api {post} /api/v1/ Employee signup
+* @api {post} /api/v1/ Sign in a user
+* @apiPermission user
+*
+* @apiParam  {String} [email] Email
+* @apiParam  {String} [first_name] First name
+* @apiParam  {String} [last_name] Last name
+* @apiParam  {String} [passwor] Password
+*
+* @apiSuccess (201) {Object} mixed `User` object
+*/
+
+import express from 'express';
+
+import controller from '../controllers/auth';
+
+const router = express.Router();
+
+// / AUTH ROUTES ///
+
+// POST Signup
+router.post('/signup', controller.signup);
+
+// POST Signin
+//router.post('/signin', controller.login);
+
+export default router;
