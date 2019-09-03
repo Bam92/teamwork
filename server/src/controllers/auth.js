c735fa2c8df98623ad6ae93fae8ee90e5f3e4086
abelmbula <abelmbula@bam.com>
2019-09-04 00:26:33 +0200

    :sparkles: ft(api):add signin feature
    Employe can sign in with an existing emaial
    
    [#168276622]

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -1,4 +1,4 @@
-import employee from '../models/employee';
+import { employee_db, getOne } from '../models/employee';
 import token from '../helpers/getToken';
 
 const Auth = {
@@ -28,15 +28,51 @@ const Auth = {
      success = true;
      status = 201;
 
-    employee.push(data)
+    employee_db.push(data)
 
-    console.log('employee: ', employee)
+    //console.log('employee: ', employee_db)
     data.token = token(email);
     delete data.password;
 
     return res.status(status).json({ status, success, message: 'User created successfully', data });
    } else {
      return res.status(status).json({ status, success, error: 'Required field missing. Try again' });
+   }
+
+  },
+
+  /**
+   * Signin an Employee
+   * @param {object} req
+   * @param {object} res
+   * @returns {object} user object
+   */
+  async signin(req, res) {
+    let success = false;
+    let status = 400;
+
+   const {
+     email,
+     password,
+      } = req.body;
+
+   if (email && password) {
+
+    if (getOne(email) !== undefined) {
+      success = true;
+      status = 200;
+      const data = getOne(email);
+
+    console.log('employee: ', getOne(email))
+    data.token = token(email);
+    delete data.password;
+
+    return res.status(status).json({ status, success, message: 'Employee signed in successfully', data });
+  } else {
+    return res.status(status).json({ status, success, error: 'Employee does not exist. Try again' });
+  }
+   } else {
+     return res.status(status).json({ status, success, error: 'Email or password not provided. Try again' });
    }
 
   }
