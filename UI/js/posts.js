f55caa435ff071377c4274cb4492d90f8e1d11ff
abelmbula <abelmbula@bam.com>
2019-09-11 15:47:48 +0200

    :lipstick: ch(ui):improve UI
    - have sticky navbar
    - change layoute of articles
    - [start] fix footer layoute issue
    - :pencil2: fix typo
    
    [#168405457]

--- UI/js/posts.js
+++ UI/js/posts.js
@@ -10,34 +10,34 @@ const posts = [
         title: 'What does a great software engineer look like?',
         tags: ['rwanda', 'andela'],
         message: '<p>I should nuance the following with a note: not all organisations make it easy for software engineers to do their job well.</p> All the advice below comes with a big caveat: if management doesn\’t do their job right, or if the organisation doesn\’t see the value of good software engineering practices, chances are you won\’t be able to be a great software engineer. That\’s a much deeper issue, and not necessarily something you can fix (aside from changing jobs, if that\’s in the realm of possibility for you). Also, a lot of this advice doesn\’t apply to solo developers. Without further ado, here are a few critical soft skills I\’d expect from a great software engineer.',
-        date: '21 Aug 2019 05:15:02 GMT',
+        date: new Date('2019-07-21 11:04:45'),
         author: 'Sarah'
     },
     {
         id: 2,
         img_url: '../images/posts/2.jpg',
         title: 'Angular basics',
-        tags: ['one', 'andela'],
-        message: 'Lorem, ipsum dolor sit amet conse elit. <p>Deserunt esse quae libero odio totam facilis, officia maxime obcaecati ipsum ad vero similique natus quidem id excepturi fugit distinctio iste corporis!</p>',
-        date: '21 Aug 2019 07:28:00 GMT',
+        tags: ['#one', '#andela'],
+        message: '<p>Lorem, ipsum dolor sit amet conse elit. Deserunt esse quae libero odio totam facilis, officia maxime obcaecati ipsum ad vero similique natus quidem id excepturi fugit distinctio iste corporis!</p>',
+        date: new Date('2019-08-21 10:14:45'),
         author: 'Patience'
     },
     {
         id: 3,
         img_url: '../images/posts/1.png',
         title: 'Starting a new job',
-        tags: ['job', 'andela', 'congo'],
+        tags: ['#job', '#andela', '#congo'],
         message: 'Lorem, ipsum <b>dolor sit amet </b>consectetur adipisicing elit. Deserunt esse quae libero odio totam facilis, officia maxime obcaecati ipsum ad vero similique natus quidem id excepturi fugit distinctio iste corporis!',
-        date: '22 Aug 2019 07:28:00 GMT',
+        date: new Date('2019-08-21 11:04:55'),
         author: 'Sarah'
     },
     {
         id: 4,
         img_url: '../images/posts/4.png',
         title: 'How to Learn Something New ',
-        tags: ['school', 'study'],
+        tags: ['#school', '#study'],
         message: 'To be successful today – in a world that is changing fast – we need to be able to consistently and efficiently adapt and grow. To achieve this we need to become a self-directed, lifelong learner. The key is discovering a love of learning. But learning doesn\’t just mean acquiring new knowledge. It\’s also about developing your skills, your attitudes, values, and your own way of being in the world.',
-        date: '25 Aug 2019 07:28:00 GMT',
+        date: new Date('2019-08-22 11:04:45'),
         author: 'Fidele'
     }
 ];
