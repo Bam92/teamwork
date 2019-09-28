646e415757f4a002e89ab13e8aec9128b988b1af
abelmbula <abelmbula@bam.com>
2019-09-29 00:19:02 +0200

    :recycle: ch(server):refactor server code base
    - use best practices: separate server with app, etc.
    - add more badges
    
    [Deliver #168723788]

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,8 @@ import "babel-polyfill";
 import './app';
 import express from 'express';
 import bodyParser from 'body-parser';
-import chalk from 'chalk';
 import morgan from 'morgan';
-import { port, endpoint } from '../../config';
+import { endpoint } from '../../config';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './docs'
 
@@ -22,14 +21,30 @@ app.use(morgan('tiny'));
 app.use(`${endpoint}/docs`, swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.post('/', (req, res) => {
-  return res.status(200).send('Welcome! The server is working properly');
+  return res.status(200).json('Welcome! The server is working properly');
 })
 
 app.use(`${endpoint}/auth`, auth);
 app.use(endpoint, articles);
 
-app.listen(port, () => {
-    console.log(`The server is listning on port ${chalk.green(port)}`);
+app.use((req, res) => {
+  const err = new Error('Not Found');
+  const status = err.status = 404;
+  res.status(status).json({
+    status,
+    success: false,
+    error: err.message
+  })
 });
+
+app.use((err, res) => {
+  const status = err.status || 500;
+  res.status(status).json({
+    status,
+    success: false,
+    error: err.message,
+  });
+});
+
 
 export default app;
